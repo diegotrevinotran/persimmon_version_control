223e1eabf2852e40e70bf84927d86a5e86020005
dtt35 <diegotrevinotran@gmail.com>
2024-12-29 01:16:24 -0800

    Update unit tests and added more test files

--- src/test/java/UnitTests.java
+++ src/test/java/UnitTests.java
@@ -1,9 +1,12 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.persimmon.Repo;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,6 +14,8 @@ import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UnitTests {
+    public static String test1DirPathString = "src" + File.separator + "test" + File.separator + "resources" + File.separator + "test1";
+    public static String test2DirPathString = "src" + File.separator + "test" + File.separator + "resources" + File.separator + "test2";
     /**
      * Given two files, return whether they are semantically equal (i.e. contains the same content) by comparing their hashcodes.
      *
@@ -20,13 +25,20 @@ public class UnitTests {
         return file1.hashCode() == file2.hashCode();
     }
 
+    /**
+     * For each test, remove all subdirectories and files within the test folder.
+     */
+    @BeforeAll
+    public static void resetAllTestDirs() {
+    }
+
     @Test
     @DisplayName("init command in empty directory")
     public void testInit1() throws IOException {
         // Set up repo in empty directory
-        String testDirPathString = "src" + File.separator + "test" + File.separator + "resources" + File.separator + "test1";
-        Path initDir = Paths.get(testDirPathString);
-        Repo initRepo = new Repo(testDirPathString);
+
+        Path initDir = Paths.get(test1DirPathString);
+        Repo initRepo = new Repo(test1DirPathString);
         initRepo.init();
         // Verify all necessary Repo directories are present
         Path initSubDir = initDir.resolve(".persimmon");
@@ -34,5 +46,22 @@ public class UnitTests {
         assert Files.exists(initSubDir.resolve("commits"));
         assert Files.exists(initSubDir.resolve("blobs"));
     }
+
+    @Test
+    @DisplayName("init command in directory with content")
+    public void testInit2() throws IOException {
+        // Set up repo in empty directory
+
+        Path initDir = Paths.get(test2DirPathString);
+        Repo initRepo = new Repo(test2DirPathString);
+        initRepo.init();
+        // Verify all necessary Repo directories are present
+        Path initSubDir = initDir.resolve(".persimmon");
+        assert Files.exists(initSubDir);
+        assert Files.exists(initSubDir.resolve("commits"));
+        assert Files.exists(initSubDir.resolve("blobs"));
+    }
+
+    // TODO add unit tests for add command
 
 }
